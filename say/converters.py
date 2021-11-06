06e81bd26cfefdcd5f49ae1df31c343e6ec257a7
Tom0666 <93554203+Tom0666@users.noreply.github.com>
2021-11-06 15:43:51 +0000

    Create converters.py

---
+++ say/converters.py
@@ -0,0 +1,37 @@
+from unidecode import unidecode
+from rapidfuzz import process
+from redbot.core import commands
+import discord
+class FuzzyRole(commands.RoleConverter):
+    """
+    This will accept role ID's, mentions, and perform a fuzzy search for
+    roles within the guild and return a list of role objects
+    matching partial names
+    """
+
+    def __init__(self, response: bool = True):
+        self.response = response
+        super().__init__()
+
+    async def convert(self, ctx: commands.Context, argument: str) -> discord.Role:
+        try:
+            basic_role = await super().convert(ctx, argument)
+        except commands.BadArgument:
+            pass
+        else:
+            return basic_role
+        guild = ctx.guild
+        result = [
+            (r[2], r[1])
+            for r in process.extract(
+                argument,
+                {r: unidecode(r.name) for r in guild.roles},
+                limit=None,
+                score_cutoff=75,
+            )
+        ]
+        if not result:
+            raise commands.BadArgument(f'Role "{argument}" not found.' if self.response else None)
+
+        sorted_result = sorted(result, key=lambda r: r[1], reverse=True)
+        return sorted_result[0][0]
